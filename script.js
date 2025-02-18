edb67e2c50138179b61f73c890e1ef3d61656e96
Ella R <ellaraputri@gmail.com>
2025-02-18 11:09:00 +0700

    move past projects to personal profile pages

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-let slideIndex = 0;
-
-function showSlides() {
-    let slides = document.querySelectorAll(".slide");
-
-    slides.forEach(slide => slide.style.display = "none"); 
-    slideIndex++;
-
-    if (slideIndex > slides.length) { slideIndex = 1; } 
-    slides[slideIndex - 1].style.display = "block"; 
-}
-
-function changeSlide(n) {
-    let slides = document.querySelectorAll(".slide");
-    slides[slideIndex - 1].style.display = "none"; 
-    slideIndex += n;
-
-    if (slideIndex > slides.length) { slideIndex = 1; }  
-    if (slideIndex < 1) { slideIndex = slides.length; }
-    slides[slideIndex - 1].style.display = "block"; 
-}
-
-showSlides();
-
-
 function reveal() {
     let finalContainer = document.querySelector(".final-container");
     let cover = document.querySelector(".cover");
